7e39ed4ca4a11256cb0954ff1a7e7624f415c982
shaotsu <72166702+shaotsuc@users.noreply.github.com>
2023-10-28 16:18:15 +0300

    Allow repeat click within index range

--- diceCraps/ViewController.swift
+++ diceCraps/ViewController.swift
@@ -1,8 +1,9 @@
 //
 //  ViewController.swift
 //  Created by Shao-Tsu on 27/Oct/2023
+//  Updated by Shao-Tsu on 28/Oct/2023
 //  Project: diceCraps
-//  Version: 2
+//  Version: 3
 //
 //
 
@@ -13,10 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftDice: UIImageView!
     @IBOutlet weak var rightDice: UIImageView!
     
-    var leftDiceNum = Int.random(in: 0..<6)
-    var rightDiceNum = Int.random(in: 0..<6)
-    var subtractNum = 1
-    
+    var leftDiceNum = Int.random(in: 0 ... 2)
+    var rightDiceNum = Int.random(in: 0 ... 2)
+    let randomNum = Int.random(in: 0 ... 2)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,23 +30,18 @@ class ViewController: UIViewController {
         
         leftDice.image = [#imageLiteral(resourceName: "diceOne"), #imageLiteral(resourceName: "diceTwo"), #imageLiteral(resourceName: "diceThree"), #imageLiteral(resourceName: "diceFour"), #imageLiteral(resourceName: "diceFive"), #imageLiteral(resourceName: "diceSix")][leftDiceNum]
         rightDice.image = [#imageLiteral(resourceName: "diceOne"), #imageLiteral(resourceName: "diceTwo"), #imageLiteral(resourceName: "diceThree"), #imageLiteral(resourceName: "diceFour"), #imageLiteral(resourceName: "diceFive"), #imageLiteral(resourceName: "diceSix")][rightDiceNum]
-        
-        if (leftDiceNum - subtractNum > 6 ||
-            rightDiceNum - subtractNum > 6) {
-            leftDiceNum = leftDiceNum - subtractNum
-            rightDiceNum = rightDiceNum - subtractNum
-        }
-        else if (leftDiceNum - subtractNum < 0 || 
-                 rightDiceNum - subtractNum < 0) {
-            leftDiceNum = leftDiceNum + subtractNum
-            rightDiceNum = rightDiceNum + subtractNum
-        }
-        // need to allow continue clicking within the number range
-        // seem needed loop
-        //for leftDiceNum in leftDice.image {
-            
-        //}
 
+        
+        leftDiceNum = leftDiceNum + randomNum
+        rightDiceNum = rightDiceNum + randomNum
+        
+        // to not get out of index range
+        if (leftDiceNum > 5 ||
+            rightDiceNum > 5) {
+            leftDiceNum = Int.random(in: 0 ... 2)
+            rightDiceNum = Int.random(in: 0 ... 2)
+        }
+        // print("left \(leftDiceNum), right \(rightDiceNum)")
     }
     
 }
