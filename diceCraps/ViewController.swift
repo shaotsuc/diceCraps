c4c538808c7c5a256d8b5eb06cfbf2c1f363b9cf
shaotsu <72166702+shaotsuc@users.noreply.github.com>
2023-10-29 20:40:29 +0200

    refactored random int and code in button

--- diceCraps/ViewController.swift
+++ diceCraps/ViewController.swift
@@ -1,48 +1,29 @@
 //
 //  ViewController.swift
 //  Created by Shao-Tsu on 27/Oct/2023
-//  Updated by Shao-Tsu on 28/Oct/2023
+//  Updated by Shao-Tsu on 29/Oct/2023
 //  Project: diceCraps
-//  Version: 3
+//  Version: 4
 //
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var leftDice: UIImageView!
     @IBOutlet weak var rightDice: UIImageView!
     
-    var leftDiceNum = Int.random(in: 0 ... 2)
-    var rightDiceNum = Int.random(in: 0 ... 2)
-    let randomNum = Int.random(in: 0 ... 2)
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        leftDice.image = #imageLiteral(resourceName: "diceOne")
-        rightDice.image = #imageLiteral(resourceName: "diceOne")
-    }
-
+    // roll button
     @IBAction func rollDiceButton(_ sender: Any) {
         
-        leftDice.image = [#imageLiteral(resourceName: "diceOne"), #imageLiteral(resourceName: "diceTwo"), #imageLiteral(resourceName: "diceThree"), #imageLiteral(resourceName: "diceFour"), #imageLiteral(resourceName: "diceFive"), #imageLiteral(resourceName: "diceSix")][leftDiceNum]
-        rightDice.image = [#imageLiteral(resourceName: "diceOne"), #imageLiteral(resourceName: "diceTwo"), #imageLiteral(resourceName: "diceThree"), #imageLiteral(resourceName: "diceFour"), #imageLiteral(resourceName: "diceFive"), #imageLiteral(resourceName: "diceSix")][rightDiceNum]
-
+        let diceArray = [#imageLiteral(resourceName: "diceOne"), #imageLiteral(resourceName: "diceTwo"), #imageLiteral(resourceName: "diceThree"), #imageLiteral(resourceName: "diceFour"), #imageLiteral(resourceName: "diceFive"), #imageLiteral(resourceName: "diceSix")]
         
-        leftDiceNum = leftDiceNum + randomNum
-        rightDiceNum = rightDiceNum + randomNum
+        leftDice.image = diceArray.randomElement()
+        rightDice.image = diceArray.randomElement()
         
-        // to not get out of index range
-        if (leftDiceNum > 5 ||
-            rightDiceNum > 5) {
-            leftDiceNum = Int.random(in: 0 ... 2)
-            rightDiceNum = Int.random(in: 0 ... 2)
-        }
-        // print("left \(leftDiceNum), right \(rightDiceNum)")
+        
     }
     
+    
 }
-
